8858f5b3a5840d989cf6f6e1345e67ae6e4ad0e5
Pierre POUCHIN <pierre.pouchin@uca.fr>
2021-03-14 23:11:53 +0100

    Only kept appropriate tests in ConnectionTest.java

--- src/test/java/fr/igred/omero/ConnectionTest.java
+++ src/test/java/fr/igred/omero/ConnectionTest.java
@@ -1,172 +1,83 @@
 package fr.igred.omero;
 
 
-import fr.igred.omero.repository.DatasetContainer;
-import fr.igred.omero.repository.ProjectContainer;
+import fr.igred.omero.exception.ServiceException;
+import fr.igred.omero.metadata.annotation.TagAnnotationContainer;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 
-public class ClientTest extends UserTest {
+public class ConnectionTest extends BasicTest {
 
 
     @Test
-    public void testProjectBasic() throws Exception {
-        ProjectContainer project = client.getProject(2L);
-
-        long id = project.getId();
-
-        assertEquals(2L, id);
-        assertEquals("TestProject", project.getName());
-        assertEquals("description", project.getDescription());
-    }
-
-
-    @Test
-    public void testGetSingleProject() throws Exception {
-        String name = client.getProject(2L).getName();
-
-        assertEquals("TestProject", name);
-    }
-
-
-    @Test
-    public void testGetAllProjects() throws Exception {
-        Collection<ProjectContainer> projects = client.getProjects();
-
-        assertEquals(2, projects.size());
-    }
-
-
-    @Test
-    public void testGetProjectByName() throws Exception {
-        Collection<ProjectContainer> projects = client.getProjects("TestProject");
-
-        int differences = 0;
-        for (ProjectContainer project : projects) {
-            if (!project.getName().equals("TestProject"))
-                differences++;
-        }
-
-        assertEquals(2, projects.size());
-        assertEquals(0, differences);
-    }
-
-
-    @Test
-    public void testDeleteProject() throws Exception {
-        client.deleteProject(client.getProject(1L));
+    public void testRootConnection() throws Exception {
+        Client testRoot = new Client();
+        testRoot.connect("omero", 4064, "root", "omero", 3L);
+        long id      = testRoot.getId();
+        long groupId = testRoot.getGroupId();
         try {
-            client.getProject(1L);
-            fail();
-        } catch (Exception e) {
-            assertTrue(true);
+            testRoot.disconnect();
+        } catch (Exception ignored) {
         }
+        assertEquals(0L, id);
+        assertEquals(3L, groupId);
     }
 
 
     @Test
-    public void testGetSingleDataset() throws Exception {
-        assertEquals("TestDataset", client.getDataset(1L).getName());
-    }
-
-
-    @Test
-    public void testGetAllDatasets() throws Exception {
-        Collection<DatasetContainer> datasets = client.getDatasets();
-        assertEquals(3, datasets.size());
-    }
-
-
-    @Test
-    public void testGetDatasetByName() throws Exception {
-        Collection<DatasetContainer> datasets = client.getDatasets("TestDataset");
-
-        int differences = 0;
-        for (DatasetContainer dataset : datasets) {
-            if (!dataset.getName().equals("TestDataset"))
-                differences++;
+    public void testUserConnection() throws Exception {
+        Client testUser = new Client();
+        testUser.connect("omero", 4064, "testUser", "password");
+        long id      = testUser.getId();
+        long groupId = testUser.getGroupId();
+        try {
+            testUser.disconnect();
+        } catch (Exception ignored) {
         }
-        assertEquals(2, datasets.size());
-        assertEquals(0, differences);
+        assertEquals(2L, id);
+        assertEquals(3L, groupId);
     }
 
 
     @Test
-    public void testGetImages() throws Exception {
-        List<ImageContainer> images = client.getImages();
-        assertEquals(4, images.size());
-    }
+    public void testSudoTag() throws Exception {
+        Client root = new Client();
+        root.connect("omero", 4064, "root", "omero", 3L);
+        assertEquals(0L, root.getId().longValue());
 
+        Client test = root.sudoGetUser("testUser");
+        assertEquals(2L, test.getId().longValue());
 
-    @Test
-    public void testGetImage() throws Exception {
-        ImageContainer image = client.getImage(1L);
-        assertEquals("image1.fake", image.getName());
-    }
+        TagAnnotationContainer tag = new TagAnnotationContainer(test, "Tag", "This is a tag");
 
-
-    @Test
-    public void testGetImagesName() throws Exception {
-        List<ImageContainer> images = client.getImages("image1.fake");
-        assertEquals(3, images.size());
-    }
-
-
-    @Test
-    public void testGetImagesLike() throws Exception {
-        List<ImageContainer> images = client.getImagesLike("image1");
-        assertEquals(3, images.size());
-    }
-
-
-    @Test
-    public void testGetImagesTagged() throws Exception {
-        List<ImageContainer> images = client.getImagesTagged(1L);
-        assertEquals(3, images.size());
-    }
-
-
-    @Test
-    public void testGetImagesKey() throws Exception {
-        List<ImageContainer> images = client.getImagesKey("testKey1");
-        assertEquals(3, images.size());
-    }
-
-
-    @Test
-    public void testGetImagesKeyValue() throws Exception {
-        List<ImageContainer> images = client.getImagesPairKeyValue("testKey1", "testValue1");
-        assertEquals(2, images.size());
-    }
-
-
-    @Test
-    public void testGetImagesCond() throws Exception {
-        String key = "testKey2";
-
-        /* Load the image with the key */
-        List<ImageContainer> images = client.getImagesKey(key);
-
-        List<ImageContainer> imagesCond = new ArrayList<>();
+        List<ImageContainer> images = test.getImages();
 
         for (ImageContainer image : images) {
-            /* Get the value for the key */
-            String value = image.getValue(client, key);
-
-            /* Condition */
-            if (value.compareTo("25") > 0) {
-                imagesCond.add(image);
-            }
+            image.addTag(test, tag);
         }
 
-        assertEquals(1, imagesCond.size());
+        List<ImageContainer> tagged = test.getImagesTagged(tag);
+
+        int differences = 0;
+        for (int i = 0; i < images.size(); i++) {
+            if (!images.get(i).getId().equals(tagged.get(i).getId()))
+                differences++;
+        }
+
+        root.deleteTag(tag);
+        try {
+            root.disconnect();
+        } catch (Exception ignored) {
+        }
+
+        assertNotEquals(0, images.size());
+        assertEquals(images.size(), tagged.size());
+        assertEquals(0, differences);
     }
 
 }
