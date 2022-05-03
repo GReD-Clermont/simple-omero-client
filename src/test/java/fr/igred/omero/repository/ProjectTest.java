730d8ebf2f23403583a34342124028ab2cf35f9a
Pierre POUCHIN <pierre.pouchin@uca.fr>
2022-05-03 17:06:49 +0200

    Add tests

--- src/test/java/fr/igred/omero/repository/ProjectTest.java
+++ src/test/java/fr/igred/omero/repository/ProjectTest.java
@@ -17,12 +17,21 @@ package fr.igred.omero.repository;
 
 
 import fr.igred.omero.UserTest;
+import fr.igred.omero.annotations.FileAnnotationWrapper;
+import fr.igred.omero.annotations.MapAnnotationWrapper;
+import fr.igred.omero.annotations.TableWrapper;
 import fr.igred.omero.annotations.TagAnnotationWrapper;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 
 public class ProjectTest extends UserTest {
@@ -279,6 +288,96 @@ public class ProjectTest extends UserTest {
         project.setDescription(description);
         project.saveAndUpdate(client);
         assertEquals(description, client.getProject(PROJECT1.id).getDescription());
+    }
+
+
+    @Test
+    public void testCopyAnnotations() throws Exception {
+        ProjectWrapper project1 = client.getProject(PROJECT1.id);
+        ProjectWrapper project2 = new ProjectWrapper(client, "CopyTest", "Copy annotations");
+
+        File file = new File("." + File.separator + "test.txt");
+        if (!file.createNewFile())
+            System.err.println("\"" + file.getCanonicalPath() + "\" could not be created.");
+
+        final byte[] array = new byte[2 * 262144 + 20];
+        new SecureRandom().nextBytes(array);
+        String generatedString = new String(array, StandardCharsets.UTF_8);
+        try (PrintStream out = new PrintStream(new FileOutputStream(file), false, "UTF-8")) {
+            out.print(generatedString);
+        }
+
+        Long fileId = project1.addFile(client, file);
+        if (!file.delete())
+            System.err.println("\"" + file.getCanonicalPath() + "\" could not be deleted.");
+        assertNotEquals(0L, fileId.longValue());
+
+        TagAnnotationWrapper tag = new TagAnnotationWrapper(client, "CopyTestTag", "Copy annotations");
+        project1.addTag(client, tag);
+        project1.addPairKeyValue(client, "CopyTest", "Annotation");
+
+        TableWrapper table = new TableWrapper(1, "CopyTest");
+        table.setColumn(0, "Name", String.class);
+        table.setRowCount(1);
+        table.addRow("Annotation");
+        project1.addTable(client, table);
+
+        project2.copyAnnotations(client, project1);
+
+        assertEquals(project1.getTags(client).size(), project2.getTags(client).size());
+        assertEquals(project1.getTables(client).size(), project2.getTables(client).size());
+        assertEquals(project1.getFileAnnotations(client).size(), project2.getFileAnnotations(client).size());
+        assertEquals(project1.getMapAnnotations(client).size(), project2.getMapAnnotations(client).size());
+
+        client.deleteFile(fileId);
+        client.delete(tag);
+        client.delete(table);
+        List<MapAnnotationWrapper> maps = project1.getMapAnnotations(client);
+        if(!maps.isEmpty())
+            for(MapAnnotationWrapper map : maps)
+                client.delete(map);
+
+        assertEquals(0, project2.getTags(client).size());
+        assertEquals(0, project2.getTables(client).size());
+        assertEquals(0, project2.getFileAnnotations(client).size());
+        assertEquals(0, project2.getMapAnnotations(client).size());
+
+        client.delete(project2);
+    }
+
+
+    @Test
+    public void testCopyFileAnnotation() throws Exception {
+        ProjectWrapper project1 = client.getProject(PROJECT1.id);
+        ProjectWrapper project2 = new ProjectWrapper(client, "CopyTest", "Copy file annotation");
+
+        File file = new File("." + File.separator + "test.txt");
+        if (!file.createNewFile())
+            System.err.println("\"" + file.getCanonicalPath() + "\" could not be created.");
+
+        final byte[] array = new byte[2 * 262144 + 20];
+        new SecureRandom().nextBytes(array);
+        String generatedString = new String(array, StandardCharsets.UTF_8);
+        try (PrintStream out = new PrintStream(new FileOutputStream(file), false, "UTF-8")) {
+            out.print(generatedString);
+        }
+
+        Long fileId = project1.addFile(client, file);
+        if (!file.delete())
+            System.err.println("\"" + file.getCanonicalPath() + "\" could not be deleted.");
+        assertNotEquals(0L, fileId.longValue());
+
+        List<FileAnnotationWrapper> files = project1.getFileAnnotations(client);
+        assertEquals(1, files.size());
+
+        if(!files.isEmpty()) {
+            project2.addFileAnnotation(client, files.get(0));
+        }
+        assertEquals(1, project2.getFileAnnotations(client).size());
+
+        client.deleteFile(fileId);
+
+        assertNotEquals(0L, fileId.longValue());
     }
 
 }
